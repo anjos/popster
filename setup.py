bd6cd9fcbc0f4f6c0e80dad0194738fd99ebb6f8
Andre Anjos <andre.dos.anjos@gmail.com>
2016-12-30 01:35:39 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name='popster',
+    version='1.0.0b0',
+    description="A pythonic photo importer for my QNAP NAS",
+    url='https://github.com/anjos/popster',
+    license="GPLv3",
+    author='Andre Anjos',
+    author_email='andre.dos.anjos@gmail.com',
+    long_description=open('README.rst').read(),
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+      'setuptools',
+      'pillow',
+      ],
+
+    entry_points = {
+      'console_scripts': [
+        'popin = popster.scripts:main',
+      ],
+    },
+
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+)
