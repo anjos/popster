eb50d1fd91b312fcf880e0e28c1b5c71da9bd83e
Andre Anjos <andre.dos.anjos@gmail.com>
2018-03-20 10:22:12 +0100

    Comment dependency building

--- scripts/conda-build-all.sh
+++ scripts/conda-build-all.sh
@@ -4,18 +4,18 @@
 conda_dir=$HOME/conda
 
 simple_pkgs=()
-simple_pkgs+=('deps/mediainfo')
+#simple_pkgs+=('deps/mediainfo')
 
 python_versions=()
 python_versions+=('2.7')
 python_versions+=('3.6')
 
 python_pkgs=()
-python_pkgs+=('deps/pymediainfo')
-python_pkgs+=('deps/exifread')
-python_pkgs+=('deps/argh')
-python_pkgs+=('deps/pathtools')
-python_pkgs+=('deps/watchdog')
+#python_pkgs+=('deps/pymediainfo')
+#python_pkgs+=('deps/exifread')
+#python_pkgs+=('deps/argh')
+#python_pkgs+=('deps/pathtools')
+#python_pkgs+=('deps/watchdog')
 python_pkgs+=('conda') #popster itself
 
 for p in "${simple_pkgs[@]}"; do
