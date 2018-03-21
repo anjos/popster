435b0b1e257d64e7d36a8914b1d397bf56af2c36
Andre Anjos <andre.dos.anjos@gmail.com>
2018-03-21 05:59:18 +0100

    Preparations for release v1.2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='popster',
-    version='1.2.3',
+    version='1.2.4',
     description="A pythonic photo importer for my QNAP NAS",
     url='https://github.com/anjos/popster',
     license="GPLv3",
